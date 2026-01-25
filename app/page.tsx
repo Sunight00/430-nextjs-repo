e6713e5befe0d1394b4f05acbc574f499740c7ed
Sunight00 <ephraim.ewane@gmail.com>
2026-01-25 21:02:34 +0100

    okay

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import desktop_image from '../public/for_now_desk.jpg'
-import mobile_image from '../public/for_now_mobile.webp'
+import desktop_image from '../public/handcrafted_haven_desktop.webp'
+import mobile_image from '../public/handcrafted_haven_mobile-removebg-preview (1).png'
 import Image from 'next/image';
 import {Button} from '../app/ui/button';
 import Fonts from '../app/ui/font';
@@ -9,15 +9,15 @@ export default function Page(){
   return <div className="w-full h-screen relative">
   {/*<!-- Image goes here -->*/}
   <Image src={desktop_image} alt="Background Image" width='2560' height='1440'
-    className="absolute inset-0 w-full h-full object-cover hidden md:block"
+    className="absolute inset-0 w-full h-full object-cover "
   />
 
-  <Image src={mobile_image}  alt="Background Image" width='640' height='960'
+  {/*<Image src={mobile_image}  alt="Background Image" width='640' height='960'
     className="absolute inset-0 w-full h-full object-cover md:hidden"
-  />
+  />*/}
   <div className="absolute inset-0 flex flex-col md:flex-row items-center justify-center text-black">
-    <h1 className={`${Fonts.Headers.className} text-center text-2xl md:text-5xl font-bold`}>Discover Unique Handcrafted Creations</h1>
-    <h2 className={`${Fonts.Headers.className} text-center text-xl md:text-3xl font-semibold mt-4 md:mt-0 `}>Support talented artisans by exploring thoughtfully made, one-of-a-kind handmade products.</h2>
+    <h1 className={`${Fonts.Headers.className} text-center text-xl md:text-5xl font-bold`}>Discover Unique Handcrafted Creations</h1>
+    <h2 className={`${Fonts.Headers.className} text-center text-l md:text-3xl font-semibold mt-4 md:mt-0 `}>Support talented artisans by exploring thoughtfully made, one-of-a-kind handmade products.</h2>
   </div>
 
   <div className="absolute bottom-8 left-0 right-0 flex flex-col md:flex-row gap-4 items-center justify-center">
--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -2,5 +2,5 @@
 
 
 export function Button({ href, description }: { href: string; description: string }) {
-    return <button onClick={() => window.location.href = href} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{description}</button>
+    return <button onClick={() => window.location.href = href} className="bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded">{description}</button>
 }
