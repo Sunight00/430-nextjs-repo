4ca2d25ea46f1691401c77b8eed28af8f6bfba84
Sunight00 <ephraim.ewane@gmail.com>
2026-01-30 03:04:37 +0100

    okay

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,12 @@
 import { getUsers } from "../lib/data"
 import {Form} from "../ui/login/form"
+import {Suspense} from 'react';
 
 export default async function Page(){
     const user = await getUsers();
     return<><h1>Login Page {user[0].name} users</h1>
+        <Suspense fallback={<div>Loading login...</div>}>
             <Form/>
+        </Suspense>
     </>
 }
