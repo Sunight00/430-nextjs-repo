6c25b88d388fe6f2977ab64116246d9787e8b653
Sunight00 <ephraim.ewane@gmail.com>
2026-02-05 11:17:53 +0100

    okay

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,7 +1,16 @@
 import postgres from 'postgres';
+import { email } from 'zod/v4/mini';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function getUsers() {
     return sql`SELECT * FROM users`;
+}
+
+export async function getUserByEmail( email: string) {
+    return sql`SELECT * FROM users WHERE email = ${email}`;
+}
+
+export async function getProductByUserId(artisanId: string){
+    return sql`SELECT * FROM products WHERE artisan_id = ${artisanId}`;
 }
--- auth.ts
+++ auth.ts
@@ -38,7 +38,8 @@ export const { auth, signIn, signOut } = NextAuth({
         const passwordsMatch = await bcrypt.compare(password, user.password);
         //const passwordsMatch = password === user.password;
             
-        if (passwordsMatch) return user;
+        if (passwordsMatch) return user
+        ;
 
         }
         return null;
